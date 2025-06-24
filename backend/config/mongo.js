ea9338edf5a00491912a9d5864cfb101994635bc
Davide Vitturini <davide.vitturini@edu.unito.it>
2025-06-24 16:22:39 +0200

    Docker compose fix for extra-hosts

--- backend/config/mongo.js
+++ backend/config/mongo.js
@@ -1,8 +1,7 @@
 // backend/config/mongo.js
 const { MongoClient } = require('mongodb');
-require('dotenv').config();
 
-const uri = process.env.MONGO_URI || 'mongodb://localhost:27017';
+const uri = process.env.MONGO_URI || 'mongodb://appointments_db:27017';
 const client = new MongoClient(uri, { useUnifiedTopology: true });
 
 let db;
